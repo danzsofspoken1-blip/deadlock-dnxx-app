2ad9199407d459336c901feef9d008c85092da56
danzsofspoken1-blip <danzsofspoken1@gmail.com>
2026-08-27 20:32:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Upload ke GitHub sebagai config.js
 window.DNXX_CONFIG = {
-  "domain": "http://deadlock-server-app-private.keselekbijiwowo.web.id:10000"
+  "domain": "http://deadlock-server-apps-private.keselekbijiwowo.web.id:10000"
 };
