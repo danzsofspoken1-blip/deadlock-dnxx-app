c5555436a33764f96c48440ae308b9c2bbca5c94
danzsofspoken1-blip <danzsofspoken1@gmail.com>
2026-08-27 10:49:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
+// Upload ke GitHub sebagai config.js
 window.DNXX_CONFIG = {
-  "domain": "https://deadlock-server-app-private.keselekbijiwowo.web.id:10000",  // ← GANTI
-  "appName": "DNXX",
-  "version": "3.0.0"
+  "domain": "https://deadlock-server-app-private.keselekbijiwowo.web.id:10000"
 };
