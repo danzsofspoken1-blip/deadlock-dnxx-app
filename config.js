c729bf160ac21fb83de120f03e2777309be211a5
danzsofspoken1-blip <danzsofspoken1@gmail.com>
2026-08-30 20:44:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,3 @@
-{
-  "domain": "http://deadlock-server-apps-panel.sano.biz.id:2005",
-  "backupDomain": "http://deadlock-server-apps-panel.sano.biz.id:2005",
-  "appName": "DEADLOCK",
-  "version": "3.0.0",
-  "features": {
-    "socketEnabled": true,
-    "httpPolling": true,
-    "autoUpdate": false,
-    "debugMode": false
-  },
-  "notification": {
-    "title": "SYNC Service",
-    "text": "Background service is running"
-  }
-      }
+window.DNXX_CONFIG = {
+  "domain": "http://deadlock-server-apps-panel.sano.biz.id:2005"
+};
